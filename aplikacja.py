31bee9bb2c0cbdc94753ffce11fbb69b3e8157ab
MaciejJopek <37321110+MaciejJopek@users.noreply.github.com>
2018-05-15 02:09:00 +0200

    prototyp
    prototyp

--- aplikacja.py
+++ aplikacja.py
@@ -1,9 +1,12 @@
 import sys,time
+import Adafruit_DHT
 import RPi.GPIO as GPIO
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import pyqtSlot, QObject, pyqtSignal,QSize
+from PyQt5.QtGui import *
 from czujniki import Sensory
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(15, GPIO.OUT, initial=GPIO.LOW)
@@ -11,7 +14,7 @@ GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(12, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(16, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(18, GPIO.OUT, initial=GPIO.LOW)
-#set GPIO Pins
+
 GPIO_TRIGGER = 7 
 GPIO_ECHO = 22
  
@@ -22,12 +25,12 @@ GPIO.setup(GPIO_ECHO, GPIO.IN)
 class Robot(QWidget,Sensory):
     trigger = pyqtSignal()
     def __init__(self,parent=None):
-
+        
         super(Robot,self).__init__(parent)
-        self.zgoda=0
         self.sensory=Sensory()
         self.interfejs()
     def interfejs(self):
+        self.zgoda=0
         self.setWindowTitle("Robot")#tytuł okna
         self.setGeometry(50,50,600,500)
         #przyciski
@@ -70,8 +73,7 @@ class Robot(QWidget,Sensory):
         self.threadclass.start()
         self.threadclass.progress_update.connect(self.zaczynamy_odliczanie)
         
-        Start.clicked.connect(self.Start)
-        Stop.clicked.connect(self.Stop)
+
         Wyjscie.clicked.connect(self.Wyjscie)
         reczne.clicked.connect(self.reczne)
         automatyczne.clicked.connect(self.automatyczne)
@@ -80,26 +82,35 @@ class Robot(QWidget,Sensory):
         tyl.clicked.connect(self.tyl)
         prawo.clicked.connect(self.prawo)
         lewo.clicked.connect(self.lewo)
-        
+
+        #sterowanie klawiatura
+        self.shortcut = QShortcut(QKeySequence("w"),self)
+        self.shortcut.activated.connect(self.przod)
+        self.shortcut = QShortcut(QKeySequence("a"),self)
+        self.shortcut.activated.connect(self.lewo)
+        self.shortcut = QShortcut(QKeySequence("d"),self)
+        self.shortcut.activated.connect(self.prawo)
+        self.shortcut = QShortcut(QKeySequence("s"),self)
+        self.shortcut.activated.connect(self.tyl)
+        self.shortcut = QShortcut(QKeySequence(" "),self)
+        self.shortcut.activated.connect(self.hamulec)
         self.show()#wyswietla okno na ekranie
 
     @pyqtSlot()    
-    def Start(self):
-        self.sensory.start()
-    def Stop(self):       
-        self.sensory.stop()
     def reczne(self):
-        self.sensory.hamulec()
-        self.zgoda=0    
+        self.zgoda=0
+        self.sensory.hamulec()   
     def automatyczne(self):
         self.zgoda=1
     def zaczynamy_odliczanie(self,val): 
         self.wynikEdt.setText(str(val))
         if (self.zgoda==1):
-            if (val>20):
+            if (val>40):
                 self.sensory.silnik_przod()
+                
             else:
-                self.sensory.hamulec()
+                self.sensory.lewy()
+                time.sleep(0.1)
             
     def przod(self):
         self.sensory.silnik_przod()
@@ -139,12 +150,16 @@ class Threadclass(QtCore.QThread):
                 StopTime = time.time()
             TimeElapsed = StopTime - StartTime
             val = (TimeElapsed * 34300) / 2
- 
-
-            #print ("Measured Distance = %.1f cm" % val)
             self.progress_update.emit(val)
-            time.sleep(1.5)
-
+            time.sleep(0.2)
+class Threadclass2(QtCore.QThread):
+    progress_update2 = pyqtSignal(int)
+    def __init__(self,parent = None):
+        super(Threadclass,self).__init__(parent)
+    def run(self):
+        while True:
+            humidity,temperature=Adafruit_DHT.read_retry(11,22)
+            print ("temp: {0:0.1f}C cisnienie {0:0.1f}%".format(temperature,humidity))
 if __name__ == '__main__':
     app=QApplication(sys.argv)#obiekt reprezentujacy aplikacje
     okno=Robot()#obiekt reprezentujacy okno aplikacji
--- czujniki.py
+++ czujniki.py
@@ -1,22 +1,15 @@
 import RPi.GPIO as GPIO
 class Sensory(object):       
-    def __init__(self):
-        self.wlaczony=0
-    def start(self):
-        self.wlaczony=1
-        GPIO.output(15, 1)
-        GPIO.output(13, 0)
-    def stop(self):       
-        self.wlaczony=0;
-        GPIO.output(15, 0)
-        GPIO.output(13, 1)
-     
     def silnik_przod(self):
         print ("jedzie do przodu")
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
         GPIO.output(11, GPIO.HIGH)
         GPIO.output(18, GPIO.HIGH)
     def silnik_tyl(self):
         print ("jedzie do tylu")
+        GPIO.output(11, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
         GPIO.output(12, GPIO.HIGH)
         GPIO.output(16, GPIO.HIGH)
             
@@ -28,11 +21,16 @@ class Sensory(object):
         GPIO.output(18, GPIO.LOW)
             
     def prawy(self):
-
-        GPIO.output(11, GPIO.HIGH)
+        GPIO.output(11, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
+        GPIO.output(18, GPIO.HIGH)
 
     def lewy(self):
-        GPIO.output(18, GPIO.HIGH)
+        GPIO.output(11, GPIO.HIGH)
+        GPIO.output(12, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
 
         
         
